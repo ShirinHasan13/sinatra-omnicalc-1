a761257ecfa2795888360bb8e8440b754b63b300
ShirinHasan13 <shirin10@chicagobooth.edu>
2024-07-12 00:11:48 +0000

    y

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ get("/square/new") do
 end
 
 get ("/square/results") do
-  @the_num = params.fetch("users_number").to_f
-  @the_result = @the_num ** 2
+  @the_num = params.fetch("users_number").to_i
+  @the_result = @the_num.to_f ** 2
   erb(:square_results)
 end
 
